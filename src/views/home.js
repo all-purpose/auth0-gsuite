3550ed63df25ab57b31a70d19407ac05d52b7172
Jason Landry <jason@inkbase.com>
2020-10-21 16:50:31 -0700

    Remove unused vars

--- src/views/home.js
+++ src/views/home.js
@@ -3,7 +3,6 @@ import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 
 import LoginButton from '../components/login-button';
-import LogoutButton from '../components/logout-button';
 
 const Home = () => {
   const { isAuthenticated } = useAuth0();
